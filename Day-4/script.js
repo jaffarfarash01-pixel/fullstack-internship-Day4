a5a7403fa59b0819d52e915d12aea05954ae97dd
jaffarfarash01-pixel <jaffarfarash01@gmail.com>
2026-07-27 22:37:52 +0530

    Add files via upload

---
+++ Day-4/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("blogForm");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const title = document.getElementById("title").value.trim();
+    const content = document.getElementById("content").value.trim();
+
+    if (title === "" || content === "") {
+        message.innerText = "Please fill all fields.";
+        message.style.color = "red";
+        return;
+    }
+
+    message.innerText = "Blog added successfully!";
+    message.style.color = "green";
+
+    form.reset();
+
+});
